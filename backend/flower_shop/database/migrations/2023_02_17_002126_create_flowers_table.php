42c7a9cb00c9a7b898bafd104764b41f8eebc493
Filip Đalic <filipdjalic2000@gmail.com>
2023-02-18 00:59:48 +0100

    artisan fix

--- backend/flower_shop/database/migrations/2023_02_17_002126_create_flowers_table.php
+++ backend/flower_shop/database/migrations/2023_02_17_002126_create_flowers_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('flowers', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('description');
+            $table->foreignId('category_id');
+
+            $table->id();
+
+            
         });
     }
 
